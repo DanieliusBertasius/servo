0c1bc9942c05f48820071edfdd6703e4b24f03f0
Danielius Bertašius <danielius.bertasius333@gmail.com>
2026-07-25 01:00:15 +0300

    Update main.h

--- software/servo/Core/Inc/main.h
+++ software/servo/Core/Inc/main.h
@@ -70,7 +70,7 @@ void Error_Handler(void);
 /* USER CODE BEGIN Private defines */
 #define DEBOUNCE 500
 #define SOFTSTART 100
-#define ADCSTART (SOFTSTART+20)
+#define ADCSTART (SOFTSTART+50)
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
